6d7c660675f4273937e94021195898309293058e
Tarik <a.tarik2005@gmail.com>
2022-10-11 13:34:08 +0300

    suprise me!

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Button from '../button/Button';
+import React, { useState } from 'react'
+
 import cartStyle from './card.module.css'
 
 import { FaChevronLeft ,
@@ -7,31 +7,64 @@ import { FaChevronLeft ,
 
 } from 'react-icons/fa';
 
+
 const Card = ({data}) => {
-    // console.log(data)
-    // const {image,id,name,job,text}=data
-   
+  //? let sayac = 1
+const [index,setIndex]=useState(0)
+ 
+const {name,id,text,image,job}=data[index]
+
+const checkNumber =(number)=>{
+ if(number> data.length -1){
+  return 0
+ }
+ if(number<0){
+  return data.length -1
+ }
+ return number
+}
+
+  const arttır=()=> {
+    setIndex((index)=>{
+      let newIndex = index +1
+      return checkNumber(newIndex)
+    })
+  }
+
+
+  
+  const azalt=()=> {
+    setIndex((index)=>{
+      let newIndex = index -1
+      return checkNumber(newIndex)
+    })
+  }
+  // ************
+
+
+ const suprise=()=>{
+  let x = Math.round(Math.random()*data.length)
+  console.log(x)
+  if(x == index){
+    x= index +1
+  }
+ setIndex( checkNumber(x))
+
+   }
+  
+  
+
   return (
-    <div>
-         {data.map((item)=>{
-            const {name,id,text,image,job}=item;
-           return(
+    <div className='main'> 
             <div className={cartStyle.container} key={id}> 
             <img src={image} alt="" />
             <h2>{name}</h2>
             <h3>{job}</h3>
             <p>{text}</p>
-            <span><FaChevronLeft /> <FaChevronRight /></span>
-            <Button/>
+            <span><FaChevronLeft style={{border:"2px solid black",height:"1.5rem",borderRadius:"0.5rem",padding:"5px",width:"28px",height:"28px"}} className='sol' onClick={azalt} /> <FaChevronRight style={{border:"2px solid black",height:"1.5rem",borderRadius:"0.5rem",padding:"5px",width:"28px",height:"28px"}} className='sag'  onClick={arttır}/></span> <br />
+            <button onClick={suprise}>Suprise Me!🐱‍🐉</button>
             </div>
            
-           )
-         
-
-         })}
-     
-        
-
     </div>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './header.module.css'
 const Header = () => {
   return (
     <div>
-        <h1>Our Reviews</h1>
+        <h1 >Our Reviews</h1>
     </div>
   )
 }
